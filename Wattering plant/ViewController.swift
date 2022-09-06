e0e9a07f87893190733eb1a4be55f2244b7b32e6
leticiadutra22-23 <leticiack.ldtra@gmail.com>
2022-09-06 11:24:57 -0300

    Initial Commit

---
+++ Wattering plant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wattering plant
+//
+//  Created by Maria Letícia Dutra de Oliveira on 06/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
