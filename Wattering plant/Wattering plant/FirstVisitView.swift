e74ddf26899efc4e6b7365b852db38325be9abf5
~leticiadutra <101335613+leticiadutra22-23@users.noreply.github.com>
2022-09-16 09:29:05 -0300

    Update FirstVisitView.swift

--- Wattering plant/Wattering plant/FirstVisitView.swift
+++ Wattering plant/Wattering plant/FirstVisitView.swift
@@ -9,33 +9,104 @@ import SwiftUI
 
 
 struct FirstVisitView: View {
+    
+    let darkgreen = Color(red: 77/255, green: 93/255, blue: 83/255)
+    let lightgreen = Color(red: 143/255, green: 151/255, blue: 121/255)
+    let darkgray = Color(red: 48/255, green: 48/255, blue: 48/255)
+    
+    @State var offset : CGSize = .zero
+    @State var showHome = false
+    
+    
     var body: some View {
+        
         NavigationView {
+            
             ZStack {
-                //Color(red: 223/255, green: 255/255, blue: 214/255).ignoresSafeArea()
+                Color(red: 77/255, green: 93/255, blue: 83/255)
+                
+                .clipShape(LiquidSwipe(offset: offset))
+                .ignoresSafeArea()
+                .overlay(
+                
+                    Image(systemName: "chevron.left")
+                        .font(.title.bold())
+                        .foregroundColor(darkgreen)
+                        .frame(width: 50, height: 50)
+                        .contentShape(Rectangle())
+                        .gesture(DragGesture().onChanged({ (value) in
+                            
+                            withAnimation(.interactiveSpring(response: 0.7, dampingFraction: 0.6, blendDuration: 0.6)){
+                                offset = value.translation
+                            }
+                            
+                        }).onEnded({(value) in
+                            
+                            let screen = UIScreen.main.bounds
+                            
+                            withAnimation(.spring()){
+                                
+                                if -offset.width > screen.width / 2 {
+                                    offset.width = -screen.height
+                                    showHome.toggle()
+                                } else {
+                                    offset = .zero
+                                }
+
+                            }
+                        }))
+                        .offset(x: 15, y: -40)
+                        .foregroundColor(.white)
+                        .opacity(offset == .zero ? 1 : 0)
+                    
+                    ,alignment: .topTrailing
+                )
+                .padding(.trailing)
+                
+                if showHome {
+                    PersonNameView()
+                }
+                
                 VStack {
-                    Image("iconSplash")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 250, height: 250)
+
                     Spacer().frame(height: 120)
-                    Text("Antes de iniciarmos, precisamos de quatro informações...")
-                        .foregroundColor(Color(red: 9/255, green: 82/255, blue: 86/255))
-                        .font(Font.custom("Helvetica Neue", size: 30))
-                        .fontWeight(.bold)
-                    Spacer().frame(height: 60)
-                    NavigationLink(destination: PersonNameView(), label: {
-                            Text("Vamos lá!")
-                                .foregroundColor(Color.white)
-                                .font(.title)
-                                .padding()
-                                .background(Color(red: 9/255, green: 82/255, blue: 86/255))
-                                .cornerRadius(40)
-                        }
-                    )
+                    Text("Antes de iniciarmos, precisamos de três informações.")
+                        .multilineTextAlignment(.leading)
+                        .foregroundColor(.white)
+                        .font(.system(size: 26, design: .serif))
+                        .padding(.trailing)
+                        .offset(x: -15)
+                        .gesture(DragGesture().onChanged({ (value) in
+                            
+                            withAnimation(.interactiveSpring(response: 0.7, dampingFraction: 0.6, blendDuration: 0.6)){
+                                offset = value.translation
+                            }
+                            
+                        }).onEnded({(value) in
+                            
+                            let screen = UIScreen.main.bounds
+                            
+                            withAnimation(.spring()){
+                                
+                                if -offset.width > screen.width / 2 {
+                                    offset.width = -screen.height
+                                    showHome.toggle()
+                                } else {
+                                    offset = .zero
+                                }
+
+                            }
+                        }))
+                        .offset(x: 15, y: -40)
+                        .foregroundColor(.white)
+                        .opacity(offset == .zero ? 1 : 0)
+                    
+                    Spacer().frame(height: 100)
+                
                 }
             }
         }
+        .navigationBarBackButtonHidden(true)
     }
 
 }
@@ -43,5 +114,40 @@ struct FirstVisitView: View {
 struct FirstVisitView_Preview : PreviewProvider {
     static var previews: some View {
         FirstVisitView()
+    }
+}
+
+struct LiquidSwipe: Shape {
+
+    var offset : CGSize
+    
+    var animatableData: CGSize.AnimatableData {
+        get{return offset.animatableData}
+        set{offset.animatableData = newValue}
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        return Path{path in
+
+            
+            let width = rect.width + (-offset.width >  0 ? offset.width : 0)
+            
+            path.move(to: CGPoint(x: 0, y: 0))
+            path.addLine(to: CGPoint(x: rect.width, y: 0))
+            path.addLine(to: CGPoint(x: rect.width, y: rect.height))
+            path.addLine(to: CGPoint(x: 0, y: rect.height))
+            
+            let from = 80 + (offset.width)
+            
+            path.move(to: CGPoint(x: rect.width, y: from > 80 ? 80 : from))
+            
+            var to = 180 + (offset.height) + (-offset.width)
+            to = to < 180 ? 180 : to
+            
+            let mid : CGFloat = 80 + ((to - 80) / 2)
+  
+            path.addCurve(to: CGPoint(x: rect.width, y: to), control1: CGPoint(x: width - 50, y: mid), control2: CGPoint(x: width - 50, y: mid))
+            
+        }
     }
 }
