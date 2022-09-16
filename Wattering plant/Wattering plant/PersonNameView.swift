dfcc1b89d92e6b5ad60fe9e1130df648870a5efc
~leticiadutra <101335613+leticiadutra22-23@users.noreply.github.com>
2022-09-16 09:30:29 -0300

    Update PersonNameView.swift

--- Wattering plant/Wattering plant/PersonNameView.swift
+++ Wattering plant/Wattering plant/PersonNameView.swift
@@ -9,42 +9,80 @@ import Foundation
 import SwiftUI
 
 struct PersonNameView: View {
-    @State var textFieldPerson: String = ""
+    
     var body: some View {
+        
         NavigationView {
-            ZStack {
-                Color(red: 223/255, green: 255/255, blue: 214/255).ignoresSafeArea()
-                VStack {
-                    Text("Qual o seu nome?")
-                        .foregroundColor(Color(red: 9/255, green: 82/255, blue: 86/255))
-                        .fontWeight(.bold)
-                        .font(.system(size: 30, design: .serif))
-                    Spacer().frame(height: 50)
-                    TextField("Seu nome ♡", text: $textFieldPerson)
-                        .padding()
-                        .font(.headline)
-                        .foregroundColor(Color(red: 223/255, green: 255/255, blue: 214/255))
-                        .background(Color.white)
-                        .frame(maxWidth: .infinity)
-                        .cornerRadius(15)
-                    Spacer().frame(height: 90)
-                    NavigationLink(destination: PlantNameView(), label: {
-                            Text("Próximo!")
-                                .foregroundColor(Color(red: 223/255, green: 255/255, blue: 214/255))
-                                .font(.system(size: 20, design: .serif))
-                                .padding()
-                                .background(Color(red: 9/255, green: 82/255, blue: 86/255))
-                                .cornerRadius(20)
-                        }
-                    )
-                }
-                .padding()
+            
+            ZStack (alignment: .top){
+                Color(.white).ignoresSafeArea()
+                
+                NameUser()
+                
             }
-        }
+
+        }.navigationBarBackButtonHidden(true)
     }
 }
+
+struct NameUser : View {
+    
+    @State var textFieldPerson: String = ""
+    
+    let darkgreen = Color(red: 77/255, green: 93/255, blue: 83/255)
+    let graygreen = Color(red: 115/255, green: 130/255, blue: 118/255)
+    
+    var body: some View {
+    
+    VStack {
+        
+        Text("Qual o seu nome?")
+            .foregroundColor(Color(red: 77/255, green: 93/255, blue: 83/255))
+            .font(.system(size: 20, design: .serif))
+
+        TextField(" ", text: $textFieldPerson)
+            .multilineTextAlignment(.center)
+            .foregroundColor(graygreen)
+            .font(.system(size: 15, design: .serif))
+            .padding()
+
+        
+        Divider()
+            
+            .background(Color(red: 77/255, green: 93/255, blue: 83/255))
+            .frame(width: 350)
+            
+        
+        
+        Spacer().frame(height: 100)
+
+        NavigationLink(destination: PlantNameView().navigationBarHidden(true), label: {
+            Text("Próximo")
+            .foregroundColor(darkgreen)
+                .padding()
+                .frame(width: 200, height: 50)
+                .font(.system(size: 18, design: .serif))
+                .overlay(RoundedRectangle(cornerRadius: 15)
+                .stroke(darkgreen, lineWidth: 2)
+
+                )
+                .padding()
+                .background(.white)
+                .cornerRadius(20)
+            }
+        )
+        
+        Spacer().frame(height: 146)
+        
+        }
+    }
+    
+}
+
 struct PersonNameView_Preview : PreviewProvider {
     static var previews: some View {
         PersonNameView()
     }
 }
+
+
