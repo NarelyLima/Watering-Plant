9b6398de4133865e8f8ab42c6dd4a4b6f0177926
leticiadutra22-23 <leticiack.ldtra@gmail.com>
2022-09-21 08:31:44 -0300

    redesign

--- Wattering plant/Wattering plant/PersonNameView.swift
+++ Wattering plant/Wattering plant/PersonNameView.swift
@@ -1,50 +1,163 @@
 //
-//  PersonNameView.swift
+//  PlantType.swift
 //  Wattering plant
 //
-//  Created by Narely Lima on 12/09/22.
+//  Created by Maria Letícia Dutra de Oliveira on 13/09/22.
 //
-
-import Foundation
+ 
 import SwiftUI
-
-struct PersonNameView: View {
-    @State var textFieldPerson: String = ""
+ 
+struct PlantType: View {
+    
+    var columns = Array(repeating: GridItem(.flexible(), spacing: 15), count: 2)
+    
+    let darkgreen = Color(red: 77/255, green: 93/255, blue: 83/255)
+    @State var selected : Plants = PlantsType[0]
+    @State var show = false
+    @Namespace var namespace
+    
+    
     var body: some View {
-        NavigationView {
-            ZStack {
-                Color(red: 223/255, green: 255/255, blue: 214/255).ignoresSafeArea()
-                VStack {
-                    Text("Qual o seu nome?")
-                        .foregroundColor(Color(red: 9/255, green: 82/255, blue: 86/255))
-                        .fontWeight(.bold)
-                        .font(.system(size: 30, design: .serif))
-                    Spacer().frame(height: 50)
-                    TextField("Seu nome ♡", text: $textFieldPerson)
-                        .padding()
-                        .font(.headline)
-                        .foregroundColor(Color(red: 223/255, green: 255/255, blue: 214/255))
-                        .background(Color.white)
-                        .frame(maxWidth: .infinity)
-                        .cornerRadius(15)
-                    Spacer().frame(height: 90)
-                    NavigationLink(destination: PlantNameView(), label: {
-                            Text("Próximo!")
-                                .foregroundColor(Color(red: 223/255, green: 255/255, blue: 214/255))
-                                .font(.system(size: 20, design: .serif))
-                                .padding()
-                                .background(Color(red: 9/255, green: 82/255, blue: 86/255))
-                                .cornerRadius(20)
-                        }
-                    )
-                }
+        
+    ZStack (alignment: .top){
+        
+            
+            Spacer().frame(width: 30, height: 30)
+            
+            .padding([.leading,.top])
+            
+        VStack {
+            
+            Text("Escolha a sua plantinha")
+            .multilineTextAlignment(.center)
+                .foregroundColor(Color(red: 77/255, green: 93/255, blue: 83/255))
+                .font(.system(size: 20, design: .rounded))
                 .padding()
+            
+            ScrollView(.vertical, showsIndicators: false){
+                
+                LazyVGrid(columns: columns, spacing: 15){
+                    
+                    ForEach(PlantsType){ plant in
+                        
+                        VStack (alignment: .center, spacing: 10){
+                            
+                            Image(plant.image)
+                                .resizable()
+                                .frame(height: 180)
+                                .cornerRadius(15)
+                                .onTapGesture {
+                                    
+                                    withAnimation(.spring()){
+                                        
+                                        show.toggle()
+                                        selected = plant
+                                        
+                                    }
+                                    
+                                }
+                                .matchedGeometryEffect(id: plant.id, in: namespace)
+                            
+                            Text(plant.type)
+                                .multilineTextAlignment(.center)
+                                .foregroundColor(Color(red: 77/255, green: 93/255, blue: 83/255))
+                                .font(.system(size: 15, design: .rounded))
+                            
+                        }
+                        
+                    }
+                    
+                }
+                .padding(.horizontal)
             }
+            
+            if show {
+                
+                VStack{
+     
+                    ZStack(alignment: Alignment(horizontal: .center, vertical: .top)){
+                            
+                        Image(selected.image)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(height: 300)
+                            .matchedGeometryEffect(id: selected.id, in: namespace)
+                        
+                        HStack {
+                            
+                            Button{
+                                
+                                withAnimation(.spring()){
+                                    
+                                    show.toggle()
+                                    
+                                }
+                                
+                            } label: {
+                            
+                                RoundedRectangle(cornerRadius: 30)
+                                    .frame(width: 300, height: 490)
+                                    .foregroundColor(.white.opacity(0))
+                            
+                            }
+                            
+                        }
+     
+                    }
+                    
+                    VStack (alignment: .center){
+                        
+                        NavigationLink (
+                            destination : PrincipalView().navigationBarHidden(true),
+                                label : {
+                                    Text ("Próximo")
+                                        .foregroundColor(darkgreen)
+                                        .frame(width: 200, height: 50)
+                                        .font(.system(size: 20, design: .rounded))
+                                        .overlay(RoundedRectangle(cornerRadius: 15)
+                                        .stroke(darkgreen, lineWidth: 2))
+                                        .padding()
+                                    
+                        })
+                            .navigationBarBackButtonHidden(true)
+                        
+                        
+                        }
+                    
+                }
+            }
+            
+
         }
     }
+        .background(.white)
+        
+    }
+    
 }
-struct PersonNameView_Preview : PreviewProvider {
+ 
+struct PlantType_Previews: PreviewProvider {
     static var previews: some View {
-        PersonNameView()
+        PlantType()
     }
 }
+ 
+struct Plants : Identifiable {
+    
+    var id: Int
+    var type: String
+    var image: String
+    
+}
+ 
+var PlantsType = [
+    
+Plants(id: 0, type: "Flor", image: "flower"),
+Plants(id: 1, type: "Suculenta", image: "plant"),
+Plants(id: 2, type: "Frutífera", image: "fruit"),
+Plants(id: 3, type: "Cacto", image: "cactus")
+ 
+]
+ 
+
+
--- Wattering plant/Wattering plant/PlantNameView.swift
+++ Wattering plant/Wattering plant/PlantNameView.swift
@@ -8,41 +8,66 @@
 import SwiftUI
 
 struct PlantNameView: View {
+    
     @State var textFieldPlant: String = ""
+    
+    let darkgreen = Color(red: 77/255, green: 93/255, blue: 83/255)
+    let graygreen = Color(red: 115/255, green: 130/255, blue: 118/255)
+    
     var body: some View {
+        
         NavigationView {
-            ZStack {
-                Color(red: 223/255, green: 255/255, blue: 214/255).ignoresSafeArea()
+            
+            ZStack (alignment: .top) {
+                Color(.white).ignoresSafeArea()
+                
                 VStack {
+                    
                     Text("Qual o nome da sua plantinha?")
-                        .foregroundColor(Color(red: 9/255, green: 82/255, blue: 86/255))
-                        .fontWeight(.bold)
-                        .font(.system(size: 30, design: .serif))
-                        .frame(maxWidth: .infinity)
-                    Spacer().frame(height: 50)
-                    TextField("Nome da sua plantinha ❃", text: $textFieldPlant)
+                        .foregroundColor(darkgreen)
+                        .font(.system(size: 20, design: .rounded))
+
+                    TextField(" ", text: $textFieldPlant)
+                        .foregroundColor(graygreen)
+                        .multilineTextAlignment(.center)
+                        .font(.system(size: 15, design: .rounded))
                         .padding()
-                        .font(.headline)
-                        .foregroundColor(Color(red: 223/255, green: 255/255, blue: 214/255))
-                        .background(Color.white)
-                        .frame(maxWidth: .infinity)
-                        .cornerRadius(15)
-                    Spacer().frame(height: 90)
-//                    NavigationLink(destination: PersonNameView(), label: {
-                            Text("Próximo!")
-                                .foregroundColor(Color(red: 223/255, green: 255/255, blue: 214/255))
-                                .font(.system(size: 20, design: .serif))
-                                .padding()
-                                .background(Color(red: 9/255, green: 82/255, blue: 86/255))
-                                .cornerRadius(20)
-//                        }
-                    //)
-                }
+
+                    
+                    Divider()
+                        
+                        .background(Color(red: 77/255, green: 93/255, blue: 83/255))
+                        .frame(width: 350)
+                        
+                    
+                    
+                    Spacer().frame(height: 100)
+
+                    NavigationLink(destination: PlantType().navigationBarHidden(true), label: {
+                        Text("Próximo")
+                        .foregroundColor(darkgreen)
+                            .padding()
+                            .frame(width: 200, height: 50)
+                            .font(.system(size: 20, design: .rounded))
+                            .overlay(RoundedRectangle(cornerRadius: 15)
+                            .stroke(darkgreen, lineWidth: 2)
+
+                            )
+                            .padding()
+                            .background(.white)
+                            .cornerRadius(20)
+                        }
+                    )
+                        }
+                
                 .padding()
+                    
+                }
+                Spacer().frame(height: 484)
             }
         }
     }
-}
+
 
 struct PlantNameView_Previews: PreviewProvider {
     static var previews: some View {
