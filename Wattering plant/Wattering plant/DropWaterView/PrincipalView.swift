6918939f31250f6c7a9c38c11ec80e2c8d4b2d9d
NarelyLima <narely148@gmail.com>
2022-09-22 11:04:52 -0300

    Screen Initial Complete

--- Wattering plant/Wattering plant/DropWaterView/PrincipalView.swift
+++ Wattering plant/Wattering plant/DropWaterView/PrincipalView.swift
@@ -23,9 +23,14 @@ struct PrincipalView: View {
     @State var isPressed: Bool = false
     @State var audioPlayer: AVAudioPlayer?
     @State var audioPlayer2: AVAudioPlayer?
+    @State var tarefa = "Esperamos que voce goste da sua primeira, de muitas, experiência com nosso aplicativo"
+    @State var dayWatering = ""
 
     let name: String
     let plantName: String
+    @State var date = Date()
+
+
 
     var rainLightningScene: SKScene {
         let scene = RainLightningScene.shared
@@ -37,95 +42,117 @@ struct PrincipalView: View {
 
     let timer = Timer.publish(every: 0.003, on: .main, in: .common).autoconnect()
     var body: some View {
-        if isSucess {
-            PrincipalView(name: name, plantName: plantName)
-        } else {
-            ZStack(alignment: .bottom) {
-
-                LinearGradient(colors: [Color(red: 223/255, green:255/255, blue: 214/255), Color(red: 9/255, green: 82/255, blue: 86/255)],
-                               startPoint: .top,
-                               endPoint: .center)
-                .ignoresSafeArea()
-                VStack {
-                    Text("Boa noite, \(name)! \(plantName) está sem ser regada há 2 dias.")
-                        .padding()
-                    LegacySceneView(scene: {
-                        let scene = SCNScene(named: modelo.modelName)!
-                        scene.background.contents = UIColor.clear
-                        return scene
-                    }(),
-                    options: [.autoenablesDefaultLighting, .allowsCameraControl])
-                    .frame(width: UIScreen.main.bounds.width / 0.9, height: UIScreen.main.bounds.height / 1.5 , alignment: .center)
-                }
-                if !isComplete {
-                    GeometryReader{ proxy in
-                        let size = proxy.size
-
-                        WaterWave(progress: progress, waveHeight: 0.025, offset: startAnimation)
-                            .fill(Color.blue)
-                            .opacity(0.5)
-                            .ignoresSafeArea()
-                            .frame(width: size.width, height: size.height, alignment: .center)
-                            .onAppear{
-                                withAnimation(.linear(duration: 9).repeatForever(autoreverses: false)){
-                                    startAnimation = size.width
-                                }
-                        }
-                    }
-                }
-                Image(systemName: "drop")
-                    .font(.system(size: 30))
-                    .foregroundColor(.blue)
-                    .padding(25)
-                    .background(.white, in: Circle())
-                    .gesture(onHoldGesture)
-
-                if isComplete {
-                    SpriteView(scene: rainLightningScene, options: [.allowsTransparency])
-                        .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight)
-                        .ignoresSafeArea()
-                        .onAppear {
-                            let soundURL2 = NSURL(fileURLWithPath: Bundle.main.path(forResource: "DoneSound", ofType: "mp3")!)
-                            do{
-                                audioPlayer2 = try AVAudioPlayer(contentsOf: soundURL2 as URL)
-                                audioPlayer2?.play()
-                            }catch {
-                                print("there was some error. The error was \(error)")
-                            }
-                        }
-                        .onAppear{
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 4.0) {
-                                self.isSucess = true
-                            }
-                        }
-                }
-        }
-            .navigationBarBackButtonHidden(true)
-            .onAppear {
-                let soundURL = NSURL(fileURLWithPath: Bundle.main.path(forResource: "WaterSound", ofType: "mp3")!)
-                do{
-                    audioPlayer = try AVAudioPlayer(contentsOf: soundURL as URL)
-                }catch {
-                    print("there was some error. The error was \(error)")
+        ZStack(alignment: .bottom) {
+            background
+            VStack {
+                Text(tarefa)
+                    .frame(width: UIScreen.main.bounds.width*0.9)
+                Text(dayWatering)
+                    .frame(width: UIScreen.main.bounds.width*0.9)
+                treeView
+            }
+            if !isComplete {
+                waterWaveView
+            }
+            button
+            if isComplete {
+                animationRainView
+                if isSucess {
+                    animationRainView
                 }
             }
-            .onReceive(timer) { _ in
-                if isPressed {
-                    withAnimation(Animation.easeInOut(duration: 2)) {
-                        if progress >= 0.5 {
-                            isComplete = true
-                            isPressed = false
-                            audioPlayer?.stop()
+        }
+        .navigationBarBackButtonHidden(true)
+        .onAppear {
+            //MUSICA DA AGUA SUBINDO
+            let soundURL = NSURL(fileURLWithPath: Bundle.main.path(forResource: "WaterSound", ofType: "mp3")!)
+            do{
+                audioPlayer = try AVAudioPlayer(contentsOf: soundURL as URL)
+            }catch {
+                print("there was some error. The error was \(error)")
+            }
+        }
+        .onReceive(timer) { _ in
+            if isPressed {
+                withAnimation(Animation.easeInOut(duration: 2)) {
+                    if progress >= 0.5 {
+                        isComplete = true
+                        isPressed = false
+                        audioPlayer?.stop()
 
-                        } else {
-                            progress += 0.0003
-                            isComplete = false
-                        }
+                    } else {
+                        progress += 0.0003
+                        isComplete = false
                     }
                 }
             }
         }
     }
+
+    var background: some View {
+        LinearGradient(colors: [Color(red: 223/255, green:255/255, blue: 214/255), Color(red: 9/255, green: 82/255, blue: 86/255)],
+                       startPoint: .top,
+                       endPoint: .center)
+        .ignoresSafeArea()
+    }
+    var treeView: some View {
+        //INSERÇÃO DO MODELO 3D
+        LegacySceneView(scene: {
+            let scene = SCNScene(named: modelo.modelName)!
+            scene.background.contents = UIColor.clear
+            return scene
+        }(),
+                        options: [.autoenablesDefaultLighting, .allowsCameraControl])
+        .frame(width: UIScreen.main.bounds.width / 0.9, height: UIScreen.main.bounds.height / 1.5 , alignment: .center)
+
+    }
+    var waterWaveView: some View {
+        GeometryReader{ proxy in
+            let size = proxy.size
+            //FUNCAO QUE PERMITE QUE A ONDA CRESÇA
+            WaterWave(progress: progress, waveHeight: 0.025, offset: startAnimation)
+                .fill(Color.blue)
+                .opacity(0.5)
+                .ignoresSafeArea()
+                .frame(width: size.width, height: size.height, alignment: .center)
+                .onAppear{
+                    withAnimation(.linear(duration: 9).repeatForever(autoreverses: false)){
+                        startAnimation = size.width
+                    }
+                }
+        }
+    }
+    var animationRainView: some View {
+        //ANIMAÇÃO DA CHUVA
+        SpriteView(scene: rainLightningScene, options: [.allowsTransparency])
+            .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight)
+            .ignoresSafeArea()
+            .onAppear {
+                //SOM DO DONE
+                let soundURL2 = NSURL(fileURLWithPath: Bundle.main.path(forResource: "DoneSound", ofType: "mp3")!)
+                do{
+                    audioPlayer2 = try AVAudioPlayer(contentsOf: soundURL2 as URL)
+                    audioPlayer2?.play()
+                }catch {
+                    print("there was some error. The error was \(error)")
+                }
+            }
+            .onAppear{
+                //DEFINE O TEMPO DE PERMANENCIA DAS GOTINHAS
+                DispatchQueue.main.asyncAfter(deadline: .now() + 4.0) {
+                    self.startAnimation = 0
+                    self.isSucess = true
+                    self.isComplete = false
+                    self.progress = 0.1
+                    self.tarefa = "Bom dia \(name)! \(plantName) está sem ser aguada desde: "
+                    self.dayWatering = "\(Date.now.formatted(.dateTime.weekday(.wide).hour().minute().second()))"
+                }
+            }
+            .onDisappear() {
+                self.isSucess = false
+            }
+    }
+
     var onHoldGesture: some Gesture {
         DragGesture(minimumDistance: 0)
             .onChanged ({ _ in
@@ -141,8 +168,15 @@ struct PrincipalView: View {
                 // parar o timer
             })
     }
+    var button: some View {
+        Image(systemName: "drop")
+            .font(.system(size: 30))
+            .foregroundColor(.blue)
+            .padding(25)
+            .background(.white, in: Circle())
+            .gesture(onHoldGesture)
+    }
 }
-
 
 
 struct PrincipalView_Previews: PreviewProvider {
